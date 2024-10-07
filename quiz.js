af55567ae25dbc8bcba17c38c3702771bb078a41
Josh <joshuakibwage23@gmail.com>
2024-10-07 11:51:30 +0300

    Update javascript

--- quiz.js
+++ quiz.js
@@ -1,11 +1,13 @@
-
 function checkAnswer() {
+
     const correctAnswer = "4";
-    const userAnswer = document.querySelector('input"quiz":checked').value;
+    
+    const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+
     if(userAnswer === correctAnswer) {
-        document.getElementById('feedback').textContent = "Correct! Well done.";
+        feedback.textContent = "Correct! Well done.";
     } else{
-        document.getElementById('feedback').textContent = "That's incorrect. Try again!";
+        feedback.textContent = "That's incorrect. Try again!";
     }
 }
 document.getElementById('submit-answer').addEventListener("click", checkAnswer);
